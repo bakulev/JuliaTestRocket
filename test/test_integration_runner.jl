fbd3d1b8e7885e7da5d16f4e1b61c2f72e420861
Anton Bakulev <abakulev@hotmail.com>
2025-08-26 20:25:41 +0200

    style: final JuliaFormatter formatting fixes
    Remove CI log files and apply final formatting adjustments:
    - Remove logs_44239161229/ directory (CI artifacts)
    - Apply final formatting fixes to test_integration_runner.jl
    - Ensure all files pass JuliaFormatter checks
    
    This completes the formatting cleanup and ensures CI format checks will pass.

--- test/test_integration_runner.jl
+++ test/test_integration_runner.jl
@@ -32,7 +32,7 @@ GLMakie.activate!()
                 :create_visualization, :setup_visualization_window,
                 :update_coordinate_display!,
                 :create_time_observable, :format_current_time,
-                :setup_keyboard_events!, :is_movement_key, :get_pressed_keys,]
+                :setup_keyboard_events!, :is_movement_key, :get_pressed_keys]
 
             for func in required_functions
                 @test isdefined(PointController, func)
